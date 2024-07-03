94007bec05d1ec1e95ec78a01c27dc84306eb6a9
Delta Mike <87529812+DELTAMIKE7@users.noreply.github.com>
2024-07-03 23:28:31 +0530

    boolean number

--- Intro/variableMe.js
+++ Intro/variableMe.js
@@ -1,2 +1,22 @@
-var story = "ye Dil Maange more";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Variable  Primitive types
+// String
+var story = 'ye Dil Maange more';
+var ok = 30;
+story.toLowerCase();
+// ok.loLowerCase()// it shows erroe it check type and gives error
 console.log(story);
+// Number
+var userId = 3433.22;
+console.log(userId);
+userId.toFixed();
+//BOolean
+var isLoggedIn = true;
+console.log(isLoggedIn);
+//any
+var hero;
+function getHero() {
+    return 'thor';
+}
+hero = getHero();
--- Intro/variableMe.ts
+++ Intro/variableMe.ts
@@ -1,7 +1,28 @@
+// Variable  Primitive types
+// String
 let story: string = 'ye Dil Maange more';
-
-
+let ok: number = 30;
+story.toLowerCase();
+// ok.loLowerCase()// it shows erroe it check type and gives error
 
 console.log(story);
 
-export {}
+// Number
+let userId = 3433.22;
+
+console.log(userId);
+userId.toFixed();
+
+//BOolean
+let isLoggedIn: boolean = true;
+console.log(isLoggedIn);
+
+//any
+
+let hero;
+
+function getHero() {
+	return 'thor';
+}
+hero = getHero();
+export {};

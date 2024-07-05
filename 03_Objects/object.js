02e9061dbf48727d482ecc9c7c20c2e2b479f976
Delta Mike <87529812+DELTAMIKE7@users.noreply.github.com>
2024-07-05 17:36:07 +0530

    type alias

---
+++ 03_Objects/object.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//This is Normal Object
+var myObject = {
+    name: 'HackerKernel',
+    email: 'hk@gmail.com',
+    isActive: true,
+};
+function createUserr(_a) {
+    var any = _a.namee;
+    return { namee: namee };
+}
+createUserr('delta');
+function createCourse() {
+    return { name: 'react', price: 7363 };
+}
+console.log(createCourse());
--- 03_Objects/object.ts
+++ 03_Objects/object.ts
@@ -5,4 +5,32 @@ const myObject = {
 	isActive: true,
 };
 
-function createUser({name:string,is})
+// function createUserr({ namee: any }) {
+// 	return { namee };
+// }
+// createUserr('delta');
+
+function createCourse(): { name: string; price: number } {
+	return { name: 'react', price: 7363 };
+}
+console.log(createCourse());
+
+// ------ type aliases ----
+// Example 1
+type User = {
+	name: string;
+	email: string;
+	isActiver: boolean;
+};
+function createPerson(user: User):User {
+    return { name: 'me', email: 'me@', isActiver: true };
+}
+createPerson({ name: 'me', email: 'me@', isActiver: true });
+
+
+//Example 2
+
+
+
+
+export {};
